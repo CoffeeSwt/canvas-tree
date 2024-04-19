463b0fc6aff77ed271fd7eaab524a7e009bdc33a
Coffeesw <897884964@qq.com>
2024-04-19 16:36:27 +0800

    ✨ feat: generate

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,91 @@ const width = ref(600)
 const height = ref(600)
 const canvasRef = ref<HTMLCanvasElement>()
 const ctx = computed(() => canvasRef.value?.getContext('2d'))
-onMounted(() => {
-  const canvas = canvasRef.value!
 
+interface Point {
+  x: number,
+  y: number
+}
+
+interface Line {
+  startPoint: Point,
+  length: number,
+  angle: number
+}
+
+const drawLine = (l: Line) => {
+  lineTo(l.startPoint, getEndPoint(l))
+}
+const getEndPoint = (l: Line) => {
+  return {
+    x: l.startPoint.x + l.length * Math.cos(l.angle),
+    y: l.startPoint.y + l.length * Math.sin(l.angle)
+  }
+}
+const pendingTasks: Function[] = []
+const setp = (l: Line, depth = 0) => {
+  const endPoint = getEndPoint(l)
+  drawLine(l)
+  const leftLine: Line = {
+    startPoint: endPoint,
+    length: l.length + (Math.random() * 5 - 5),
+    angle: l.angle - 0.3 * Math.random()
+  }
+
+
+  const rightLine: Line = {
+    startPoint: endPoint,
+    length: l.length + (Math.random() * 5 - 5),
+    angle: l.angle + 0.3 * Math.random()
+  }
+  if (depth >= 30) return
+  if (Math.random() < 0.4 || depth < 4) {
+    pendingTasks.push(() => {
+      setp(leftLine, depth + 1)
+    })
+
+  }
+  if (Math.random() < 0.4 || depth < 4) {
+    pendingTasks.push(() => {
+      setp(rightLine, depth + 1)
+    })
+  }
+}
+
+const frame = () => {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let renderCount = 0
+const render = () => {
+  requestAnimationFrame(() => {
+    if (renderCount % 3 == 0)
+      frame()
+    render()
+    renderCount++
+  })
+}
+
+const lineTo = (startPoint: Point, endPoint: Point) => {
+  ctx.value?.beginPath()
+  ctx.value?.moveTo(startPoint.x, startPoint.y)
+  ctx.value?.lineTo(endPoint.x, endPoint.y)
+  ctx.value?.stroke()
+}
+
+const init = () => {
+  const line: Line = {
+    startPoint: { x: width.value / 2, y: height.value },
+    length: 20,
+    angle: - Math.PI / 2
+  }
+  setp(line)
+  render()
+}
+onMounted(() => {
+  init()
 })
 </script>
 
